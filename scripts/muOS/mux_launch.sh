3fab2d4a92151d52d361b8d4079600a4f86c2414
pawndev <coquelet.c@gmail.com>
2026-01-21 10:17:24 +0100

    feat(muos): add app icon (but draft, not working actually)

--- scripts/muOS/mux_launch.sh
+++ scripts/muOS/mux_launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # HELP: Grout
-# ICON: retroarch
-# GRID: RetroArch
+# ICON: grout
+# GRID: grout
 
 . /opt/muos/script/var/func.sh
 echo app >/tmp/act_go
@@ -10,6 +10,9 @@ GOV_GO="/tmp/gov_go"
 [ -e "$GOV_GO" ] && cat "$GOV_GO" >"$(GET_VAR "device" "cpu/governor")"
 
 APP_DIR="$(dirname "$0")"
+ICON_DIR=/opt/muos/share/theme/active/glyph/muxapp/
+cp "${APP_DIR}/resources/grout.png" "${ICON_DIR}/grout.png"
+
 cd "$APP_DIR" || exit 1
 
 export CFW=MUOS
