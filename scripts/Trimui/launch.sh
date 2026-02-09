449d74eddb7bef031b7148127c77b979925b39b5
Ivan <malkavi@users.noreply.github.com>
2026-02-09 21:30:30 +0100

    Update launch.sh

--- scripts/Trimui/launch.sh
+++ scripts/Trimui/launch.sh
@@ -3,6 +3,6 @@ CUR_DIR="$(dirname "$0")"
 cd "$CUR_DIR"/grout || exit 1
 
 export CFW=TRIMUI
-export LD_LIBRARY_PATH=$CUR_DIR/grout/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CUR_DIR/lib:$LD_LIBRARY_PATH
 
 ./grout
