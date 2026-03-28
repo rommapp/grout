7104be02c9b24fa3138413901dfc8016ec8c49a1
Brandon T. Kowalski <brandon@kowalski.io>
2026-03-28 09:40:19 -0400

    fix: clean up Spruce universal distribution
    Fix unmapped devices (TrimUI, Miyoo Flip) returning errors instead of
    nil. Uppercase device constants to match env var values. Add fallback
    case to launch script. Fix artifact download paths in CI universal
    packaging job. Remove debug comments and cosmetic issues.

--- cfw/spruce/input_mappings.go
+++ cfw/spruce/input_mappings.go
@@ -17,53 +17,50 @@ var embeddedInputMappings embed.FS
 type Device string
 
 const (
-	DeviceA30       Device = "a30"
-	DeviceMiyoomini Device = "miyoomini"
-	DeviceMiyooFlip Device = "miyoomini-flip"
-	DeviceTrimui    Device = "trimui"
-	DeviceUnknown   Device = "unknown"
+	DeviceA30       Device = "A30"
+	DeviceMiyooMini Device = "MIYOOMINI"
+	DeviceMiyooFlip Device = "MIYOOFLIP"
+	DeviceTrimui    Device = "TRIMUI"
+	DeviceUnknown   Device = "UNKNOWN"
 )
 
 // DetectDevice detects the device type when running on Spruce by checking environment variables.
 func DetectDevice() Device {
 	logger := gaba.GetLogger()
-	logger.Info("Detecting spruce device type...")
+	logger.Debug("Detecting Spruce device type", "env", DeviceType)
 
 	switch os.Getenv(DeviceType) {
 	case "A30":
 		return DeviceA30
 	case "MIYOOMINI":
-		return DeviceMiyoomini
+		return DeviceMiyooMini
 	case "MIYOOFLIP":
 		return DeviceMiyooFlip
 	case "TRIMUI":
 		return DeviceTrimui
 	default:
-		logger.Warn("Unknown device type, defaulting to miyoomini")
+		logger.Warn("Unknown Spruce device type", "value", os.Getenv(DeviceType))
 		return DeviceUnknown
 	}
 }
 
-// GetInputMappingBytes returns the embedded input mapping JSON for the detected muOS device
+// GetInputMappingBytes returns the embedded input mapping JSON for the detected Spruce device.
 func GetInputMappingBytes() ([]byte, error) {
 	device := DetectDevice()
 	return GetInputMappingBytesForDevice(device)
 }
 
-// GetInputMappingBytesForDevice returns the embedded input mapping JSON for a specific device
+// GetInputMappingBytesForDevice returns the embedded input mapping JSON for a specific device.
 func GetInputMappingBytesForDevice(device Device) ([]byte, error) {
 	var filename string
 	switch device {
-	case DeviceMiyoomini:
+	case DeviceMiyooMini:
 		filename = "input_mappings/miyoo.json"
 	case DeviceA30:
 		filename = "input_mappings/a30.json"
-		//case DeviceTrimui:
-		//	filename = "input_mappings/trimui.json"
-	}
-
-	if filename == "" {
-		return nil, fmt.Errorf("no input mapping found for %s", device)
+	default:
+		// TrimUI, Miyoo Flip, and unknown devices use standard SDL controller input
+		return nil, nil
 	}
 
 	overridePath := filepath.Join("overrides", "cfw", "spruce", filename)
--- scripts/Spruce/launch.sh
+++ scripts/Spruce/launch.sh
@@ -16,15 +16,9 @@ case "$PLATFORM" in
 # A30
 ############################################################
     "A30" )
-        echo "A30 detected, setting up environment variables for SDL2"
         export LD_LIBRARY_PATH="/mnt/SDCARD/spruce/a30/sdl2:$LD_LIBRARY_PATH"
-        # TODO: try to keep miyoo, but add the subdirectory `sdl2` like in the spruce a30
-        # so... even If i take the content of the spruce a30/sdl2 folder, it does not launch
-        # I really don't understand why
-#        export LD_LIBRARY_PATH="$CUR_DIR/grout/lib32/miyoo:$LD_LIBRARY_PATH"
         export LD_LIBRARY_PATH="$CUR_DIR/grout/lib32/a30:$LD_LIBRARY_PATH"
         export SPRUCE_DEVICE="A30"
-
         ./grout32
     ;;
 
@@ -32,7 +26,6 @@ case "$PLATFORM" in
 # Brick / SmartPro / SmartProS
 ############################################################
     "Brick" | "SmartPro" | "SmartProS" )
-        echo "Brick/SmartPro/SmartProS detected, setting up environment variables for SDL2"
         export LD_LIBRARY_PATH="$CUR_DIR/grout/lib64:$LD_LIBRARY_PATH"
         export SPRUCE_DEVICE="TRIMUI"
         ./grout64
@@ -42,27 +35,29 @@ case "$PLATFORM" in
 # Miyoo Flip
 ############################################################
     "Flip" )
-        echo "Miyoo Flip detected, setting up environment variables for SDL2"
         export LD_LIBRARY_PATH="$CUR_DIR/grout/lib64:$LD_LIBRARY_PATH"
         export SPRUCE_DEVICE="MIYOOFLIP"
         ./grout64
     ;;
+
 ############################################################
 # Miyoo Mini Flip
 ############################################################
     "MiyooMini" )
-        echo "Miyoo Mini detected, setting up environment variables for SDL2"
-
         export SDL_VIDEODRIVER=mmiyoo
         export SDL_AUDIODRIVER=mmiyoo
         export EGL_VIDEODRIVER=mmiyoo
         export SDL_MMIYOO_DOUBLE_BUFFER=1
-
         export IS_MIYOO=1
         export SPRUCE_DEVICE="MIYOOMINI"
         export LD_LIBRARY_PATH="$CUR_DIR/grout/lib32/miyoo:$LD_LIBRARY_PATH"
-
-
         ./grout32
+    ;;
+
+############################################################
+# Unknown
+############################################################
+    * )
+        echo "Unknown Spruce platform: $PLATFORM" >> grout.log
     ;;
 esac
